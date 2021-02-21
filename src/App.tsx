74c629794a35b7243fa310a31583fc259f5be136
olegvalujev <oleg.valujev@gmail.com>
2021-02-21 21:20:41 +0200

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ProductList from './components/ProductList/ProductList'
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+    background-color: blue;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppWrapper>
+      <ProductList/>
+    </AppWrapper>
+  )
 }
 
 export default App;
