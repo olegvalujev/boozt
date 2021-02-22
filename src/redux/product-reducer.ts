9316562f1cf730e880e0af658be7bf07ee5e68f3
olegvalujev <oleg.valujev@gmail.com>
2021-02-22 22:05:07 +0200

    feature: add product reducer and base actions, initial state

---
+++ src/redux/product-reducer.ts
@@ -0,0 +1,57 @@
+import {BaseThunkType, InferActionsTypes} from "./redux-store";
+import {productAPI} from "../api/api";
+
+const productReducer = (state = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'SN/PRODUCT/LOAD_PRODUCTS':
+            return {
+                ...state,
+                products: [...action.products]
+            }
+        default:
+            return state
+    }
+}
+
+export const actions = {
+    requestProducts: (products: Array<ProductType>) => ({type: 'SN/PRODUCT/LOAD_PRODUCTS', products: products} as const),
+    toggleIsFetching: (isFetching: boolean) => ({type: 'SN/PRODUCT/TOGGLE_IS_FETCHING', isFetching} as const),
+    setProducts: (products: Array<ProductType>) => ({type: 'SN/PRODUCT/SET_PRODUCTS', products} as const),
+    setCurrentPage: (currentPage: number) => ({type: 'SN/PRODUCT/SET_CURRENT_PAGE', currentPage} as const),
+}
+
+export const requestProducts = (page: number, pageSize: number): ThunkType => async (dispatch) =>  {
+    dispatch(actions.toggleIsFetching(true))
+    const products = await productAPI.getProducts()
+    dispatch(actions.toggleIsFetching(false))
+
+    dispatch(actions.setProducts(products))
+
+    // Promise.all([promise])
+    //     .then(() => {
+    //         dispatch(actions.initializationSuccess())
+    //     })
+}
+
+export default productReducer
+
+export type ProductType = {
+    id: string
+    product_name: string
+    brand_name: string
+    actual_price: string
+    base_price: string
+    filename: string
+}
+
+const initialState = {
+    products: [] as Array<ProductType>,
+    pageSize: 10,
+    totalProductsCount: 0,
+    currentPage: 1,
+    isFetching: true
+}
+
+type InitialStateType = typeof initialState
+type ActionsType = InferActionsTypes<typeof actions>
+type ThunkType = BaseThunkType<ActionsType>
