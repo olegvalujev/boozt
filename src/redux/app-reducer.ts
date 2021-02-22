74272b0faf5046c561edf4d142af528b02334d38
olegvalujev <oleg.valujev@gmail.com>
2021-02-22 15:28:40 +0200

    feature: add base app reducer

---
+++ src/redux/app-reducer.ts
@@ -0,0 +1,34 @@
+import {BaseThunkType, InferActionsTypes} from "./redux-store"
+
+let initialState = {
+    initialized: false
+}
+const appReducer = (state = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'SN/APP/INITIALIZATION_SUCCESS':
+            return {
+                ...state,
+                initialized: true
+            }
+        default:
+            return state
+    }
+}
+
+export const actions = {
+    initializationSuccess: () => ({type: 'SN/APP/INITIALIZATION_SUCCESS'} as const)
+}
+
+export const initializeApp = (): ThunkType => async (dispatch) =>  {
+    dispatch(actions.initializationSuccess())
+    // Promise.all([promise])
+    //     .then(() => {
+    //         dispatch(actions.initializationSuccess())
+    //     })
+}
+
+export default appReducer
+
+export type InitialStateType = typeof initialState
+type ActionsType = InferActionsTypes<typeof actions>
+type ThunkType = BaseThunkType<ActionsType>
