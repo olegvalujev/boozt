0fe3d231e373f53c4eede1c873dc94c1cef4dc68
olegvalujev <oleg.valujev@gmail.com>
2021-02-22 15:32:10 +0200

    feature: add props and store to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,69 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ProductList from './components/ProductList/ProductList'
+import {BrowserRouter, Route, Switch, withRouter} from 'react-router-dom'
 import styled from 'styled-components'
+import {connect, Provider} from 'react-redux'
+import {compose} from 'redux'
+import store, {AppStateType} from './redux/redux-store'
+import {initializeApp} from "./redux/app-reducer";
+import Preloader from "./components/common/Preloader/Preloader";
+
+type MapPropsType = ReturnType<typeof mapStateToProps>
+type DispatchPropsType = {
+    initializeApp: () => void
+}
+
+class App extends Component<MapPropsType & DispatchPropsType> {
+    catchUnhandledErrors(event: PromiseRejectionEvent) {
+        alert(`UNHANDLED PROMISE REJECTION: ${event.reason}`);
+    }
+    componentDidMount() {
+        this.props.initializeApp()
+        window.addEventListener("unhandledrejection", this.catchUnhandledErrors.bind(this));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("unhandledrejection", this.catchUnhandledErrors.bind(this))
+    }
+
+    render() {
+        if (!this.props.initialized) return <Preloader/>
+        return (
+            <AppWrapper>
+                <div className="app-wrapper-content">
+                    <Switch>
+                        <Route exact path="/" render={ProductList}/>
+                        <Route path="*">
+                            <div>404 NOT FOUND</div>
+                        </Route>
+                    </Switch>
+                </div>
+            </AppWrapper>
+        )
+    }
+}
+
+const mapStateToProps = (state: AppStateType) => ({
+    initialized: state.app.initialized
+})
+
+const AppContainer = compose<React.ComponentType>(
+    withRouter,
+    connect(mapStateToProps, {initializeApp}),
+)(App)
+
+const RootContainer: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <Provider store={store}>
+                <AppContainer/>
+            </Provider>
+        </BrowserRouter>
+    )
+}
+
+export default RootContainer;
 
 const AppWrapper = styled.div`
     background-color: blue;
 `
-
-function App() {
-  return (
-    <AppWrapper>
-      <ProductList/>
-    </AppWrapper>
-  )
-}
-
-export default App;
