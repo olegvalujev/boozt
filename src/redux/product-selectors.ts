206bc9d8a5e9b733ed7eb45ac8f3a24ecbf3f91d
olegvalujev <oleg.valujev@gmail.com>
2021-02-23 11:59:12 +0200

    feature: add base product reducer state selectors

---
+++ src/redux/product-selectors.ts
@@ -0,0 +1,22 @@
+import {createSelector} from 'reselect'
+import {AppStateType} from "./redux-store";
+import {ProductType} from './product-reducer'
+
+export const getProducts = (state: AppStateType): Array<ProductType> => {
+    return state.products.products
+}
+export const getFiltered = createSelector(getProducts, (products) => {
+    return products.filter(product => true)
+})
+export const getPageSize = (state: AppStateType) => {
+    return state.products.pageSize
+}
+export const getTotalUsersCount = (state: AppStateType) => {
+    return state.products.totalProductsCount
+}
+export const getCurrentPage = (state: AppStateType) => {
+    return state.products.currentPage
+}
+export const getIsFetching = (state: AppStateType) => {
+    return state.products.isFetching
+}
