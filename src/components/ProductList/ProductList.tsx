fbf6b6ee65a184871936841af6110aff59d7abc5
olegvalujev <oleg.valujev@gmail.com>
2021-02-23 12:47:09 +0200

    feature: add paginator component to a Product list

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,14 +1,35 @@
 import React from 'react'
 import Product from './Product/Product'
 import styled from "styled-components";
+import Paginator from "../common/Paginator/Paginator";
+import {ProductType} from "../../redux/product-reducer";
 
-const ProductListWrapper = styled.div`
-    background-color: green;
-`
-const ProductList = () => {
+const ProductList: React.FC<PropsType> = ({totalProductsCount, pageSize, onPageChanged, currentPage, products, ...props}) => {
     return <ProductListWrapper>
-        <Product/>
+        <Paginator totalProductsCount={totalProductsCount}
+                   pageSize={pageSize}
+                   onPageChanged={onPageChanged}
+                   currentPage={currentPage}
+                   portionSize={10}/>
+        <List>
+            {products.map(p => (<Product key={p.id} product={p}/>))}
+        </List>
     </ProductListWrapper>
 }
 
 export default ProductList
+
+const ProductListWrapper = styled.div`
+    background-color: green;
+`
+const List = styled.div`
+    background-color: yellow;
+`
+
+type PropsType = {
+    totalProductsCount: number
+    pageSize: number
+    onPageChanged: (pageNumber: number) => void
+    currentPage: number
+    products: Array<ProductType>
+}
